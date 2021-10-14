eeed60e695a11df00e33812aa7f1a24989ec7091
Pierre Castanha <adv.pierrecastanha@gmail.com>
2021-10-13 22:36:07 -0300

    Atualizando e inserindo contador e nome do aluno

--- atividade.js
+++ atividade.js
@@ -1,22 +1,20 @@
-let numeroAlunos = 6
+let numeroAlunos = 10
 
-let listaAlunos = ["Pierre", "Frederico", "Paulo", "Ricardo", "Amaro", "Erick"];
+let listaAlunos = ["Pierre", "Frederico", "Paulo", "Ricardo", "Amaro", "Erick", "Bernardo", "Antonio", "Artur", "Francisco"];
 
-//for (let contador = 0; contador < numeroAlunos; contador++) {
 
-    for (let contador = 0; contador < listaAlunos.length; contador++) {
-        console.log(contador)
+for (let contador = 0; contador < listaAlunos.length; contador++) {
+    console.log(contador)
 
-        if (contador == 0) {
-            //    console.log("zero")
-            //    console.log("O número é: " + contador + " Ele é Zero")
-            console.log(`${contador}: Zero`);
-            //  } else if(contador % 2 == 1) {
-        } else if (contador % 2 != 0) {
-            //    console.log("Ímpar")
-            console.log("O número é: " + contador + " Ele é Ímpar")
-        } else {
-            //  console.log("Par")
-            console.log("O número é: " + contador + " Ele é  Par")
-        }
+    if (contador == 0) {
+        console.log("o Primeiro campo da Matriz  é: " + `${contador}` + "  - Ele é um número NEUTRO  - " + "Aluno Matriculado:  " + listaAlunos[contador] + "   - " + `${contador + 1}` + "º Aluno  Cadastrado no sistema!");
+
+    } else if (contador % 2 == 1) {
+
+        console.log("o Primeiro campo da Matriz  é: " + `${contador}` + "  - Ele é um número ÍMPAR  -  " + "Aluno Matriculado:  " + listaAlunos[contador] + "   - " + `${contador + 1}` + "º Aluno  Cadastrado no sistema!");
+
+    } else {
+
+        console.log("o Primeiro campo da Matriz  é: " + `${contador}` + "  - Ele é um número PAR    - " + " Aluno Matriculado:  " + listaAlunos[contador] + "    - " + `${contador + 1}` + "º Aluno  Cadastrado no sistema!");
     }
+}
