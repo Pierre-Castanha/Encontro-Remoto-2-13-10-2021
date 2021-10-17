2c11641b92d1242e5564a57ec53f2b96ee548ff6
Pierre-Castanha <91088248+Pierre-Castanha@users.noreply.github.com>
2021-10-17 12:39:38 -0300

    Update atividade.js

--- atividade.js
+++ atividade.js
@@ -30,10 +30,10 @@ for (let contador = 0; contador < listaAlunos.length; contador++) {
 
     } else if (contador % 2 == 1) {
 
-        console.log("o Primeiro campo da Matriz  é: " + `${contador}` + "  - Ele é um número ÍMPAR  -  " + "Aluno Matriculado: " + listaAlunos[contador] + "   - " + `${contador + 1}` + "º Aluno  Cadastrado no sistema!");
+        console.log("O campo da Matriz  é: " + `${contador}` + "  - Ele é um número ÍMPAR  -  " + "Aluno Matriculado: " + listaAlunos[contador] + "   - " + `${contador + 1}` + "º Aluno  Cadastrado no sistema!");
 
     } else {
 
-        console.log("o Primeiro campo da Matriz  é: " + `${contador}` + "  - Ele é um número PAR    - " + " Aluno Matriculado: " + listaAlunos[contador] + "    - " + `${contador + 1}` + "º Aluno  Cadastrado no sistema!");
+        console.log("O campo da Matriz  é: " + `${contador}` + "  - Ele é um número PAR    - " + " Aluno Matriculado: " + listaAlunos[contador] + "    - " + `${contador + 1}` + "º Aluno  Cadastrado no sistema!");
     }
 }
